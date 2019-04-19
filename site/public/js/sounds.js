ac63a7f0cc7468c430975dd43193fcedc9f557ef
github username <github email address>
2019-04-19 09:25:01 -0400

    tested rackup

--- site/public/js/sounds.js
+++ site/public/js/sounds.js
@@ -3,31 +3,31 @@ const sounds = {
 
       playBombSound: () => {
         const bombSound = new Audio()
-        bombSound.src = "public/sounds/bomb2.mp3"
+        bombSound.src = "sounds/bomb2.mp3"
         bombSound.play()
       },
 
       playTileSound: () => {
         const tileSound = new Audio()
-        tileSound.src = "public/sounds/tile.mp3"
+        tileSound.src = "sounds/tile.mp3"
         tileSound.play()
       },
 
       playWordSound: () => {
         const wordSound = new Audio()
-        wordSound.src = "public/sounds/wordsubmit.mp3"
+        wordSound.src = "sounds/wordsubmit.mp3"
         wordSound.play()
       },
 
       playInvalidWordSound: () => {
         const invalidWordSound = new Audio()
-        invalidWordSound.src = "public/sounds/invalid.mp3"
+        invalidWordSound.src = "sounds/invalid.mp3"
         invalidWordSound.play()
       },
 
       playGameOverSound: () => {
         const gameOver = new Audio()
-        gameOver.src = "public/sounds/buzzer.mp3"
+        gameOver.src = "sounds/buzzer.mp3"
         gameOver.play()
       },
 
